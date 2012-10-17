a1c84ebfd5869bc63270c93594c366d7bb8cc2d7
Tom Nealon <tgnealon@gmail.com>
2012-10-17 19:42:54 +0100

    Finished hospitalmanager class

--- HospitalManager.java
+++ HospitalManager.java
@@ -9,26 +9,40 @@ package day6;
  * @author tom
  */
 public class HospitalManager {
-    private Patient patientListStart = null;
-    // Make patientListEnd public so it can be accessed by Patient objects.
+    // Make List End and List Start public so it can be accessed by Patient objects.
+    public Patient patientListStart = null;
     public Patient patientListEnd = null;
     
     public void launch() {
-        // Patinets are added directly to the data structure without names
-        // because we only access them through the list methods.
-        patientListStart = new Patient("John", 33, "Tuberculosis", this);
-        patientListStart.addPatient(new Patient("Bill", 33, "Tuberculosis", this));
-        patientListStart.addPatient(new Patient("Jim", 23, "Tuberculosis", this));
-        patientListStart.addPatient(new Patient("Kim", 13, "Meningitis", this));
-        patientListStart.addPatient(new Patient("Sally", 27, "Tuberculosis", this));
-        patientListStart.addPatient(new Patient("Luke", 54, "Tuberculosis", this));
-        patientListStart.addPatient(new Patient("Sue", 34, "Tuberculosis", this));
-        patientListStart.addPatient(new Patient("Ralph", 33, "Meningitis", this));
-        patientListStart.addPatient(new Patient("Ruby", 21, "Tuberculosis", this));
-        patientListStart.addPatient(new Patient("Alice", 19, "Tuberculosis", this));
+        Patient one = new Patient("John", 33, "Tuberculosis", this);
+        Patient two = new Patient("Bill", 33, "Tuberculosis", this);
+        Patient three = new Patient("Jim", 23, "Tuberculosis", this);
+        Patient four = new Patient("Kim", 13, "Meningitis", this);
+        Patient five = new Patient("Sally", 27, "Tuberculosis", this);
+        Patient six = new Patient("Luke", 54, "Tuberculosis", this);
+        Patient seven = new Patient("Sue", 34, "Tuberculosis", this);
+        Patient eight = new Patient("Ralph", 33, "Meningitis", this);
+        Patient nine = new Patient("Ruby", 21, "Tuberculosis", this);
+        Patient ten = new Patient("Alice", 19, "Tuberculosis", this); 
+        patientListStart = one; 
+        patientListStart.addPatient(two);
+        patientListStart.addPatient(three);
+        patientListStart.addPatient(four);
+        patientListStart.addPatient(five);
+        patientListStart.addPatient(six);
+        patientListStart.addPatient(seven);
+        patientListStart.addPatient(eight);
+        patientListStart.addPatient(nine);
+        patientListStart.addPatient(ten);
         
         patientListStart.forwardPrint();
         
+        patientListEnd.backPrint();
+        
+        five.deletePatient();
+        one.deletePatient();
+        ten.deletePatient();
+        patientListStart.forwardPrint();
         patientListEnd.backPrint();
     }
     
