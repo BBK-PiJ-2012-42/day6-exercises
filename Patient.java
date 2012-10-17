8e0217932f20febfac71b2e6470749b7f2556494
Tom Nealon <tgnealon@gmail.com>
2012-10-17 18:12:16 +0100

    Update

--- Patient.java
+++ Patient.java
@@ -9,27 +9,44 @@ package day6;
  * @author tom
  */
 public class Patient {
+    
+    // The controlling manager.
+    private HospitalManager patientManager;
+            
     private String name;
     private int age;
     private String illness;
     private Patient prevPatient;
     private Patient nextPatient;
     
-    public Patient(String name, int age, String illness) {
+    public Patient(String name, int age, String illness, HospitalManager manager) {
         this.name = name;
         this.age = age;
         this.illness = illness;
         this.prevPatient = null;
         this.nextPatient = null;
+        // Link to HospitalMnager Object.
+        this.patientManager = manager;
+    }
+    
+    // Method to allow setting of prevPatient
+    public void setPrevious(Patient oldPatient) {
+        prevPatient = oldPatient;
     }
     
     
     public void addPatient(Patient newPatient) {
         if (this.nextPatient == null) {
+                // At the end of the list set previousPatient to the patient at
+                // the end of the list.
+                newPatient.setPrevious(this);
+                // Then add the new patient to the end.
                 this.nextPatient = newPatient;
+                // Finally set end of the list to the new patient.
+                patientManager.patientListEnd = newPatient;
             } else {
-            this.nextPatient.addPatient(newPatient);
-        }
+                this.nextPatient.addPatient(newPatient);
+            }
     }
 
     public boolean deletePatient(Patient patient) {
@@ -40,6 +57,34 @@ public class Patient {
             return true;
         } else {
             return this.nextPatient.deletePatient(patient);
+        }
+    }
+    
+    public void printPatient() {
+        System.out.println(name+"\t"+age+"\t"+illness);
+    }
+    
+    public void forwardPrint() {
+        // Prints patient details and then goes forward in the list
+        // printing the next patient details recursively.
+        this.printPatient();
+        if (this.nextPatient == null) {    
+            System.out.println("End of patient list.");            
+        } else {
+            this.nextPatient.forwardPrint();
+        }
+    }
+    
+    
+    
+    public void backPrint() {
+        // Prints patient details and then goes backward in the list
+        // printing the previous patient details recursively.
+        this.printPatient();
+        if (this.prevPatient == null) {    
+            System.out.println("Begining of patient list.");            
+        } else {
+            this.prevPatient.backPrint();
         }
     }
     
