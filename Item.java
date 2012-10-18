7d29f36afd530c3966d141aa195fce21c639e1b7
Tom Nealon <tgnealon@gmail.com>
2012-10-18 14:54:43 +0100

    Queue example commit.

---
+++ Item.java
@@ -0,0 +1,17 @@
+package day6;
+
+/**
+ *
+ * @author tnealo01
+ */
+public class Item {
+    
+    public int value = 0;
+    public Item next = null;
+    
+    public Item (int value) {
+        this.value  = value;
+    }
+    
+    
+}
